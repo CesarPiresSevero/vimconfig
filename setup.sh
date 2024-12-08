0f68a9834041249c7d95d9557aa0a0b0e31113e5
CesarPiresSevero <cesar.pires.severo@gmail.com>
2024-12-08 16:30:06 +0100

    Created shell script for setting up vimconfig automatically

--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
-echo My script
+echo ----- VimConfig Script ----- 
+echo - Copying .vimrc
+cp .vimrc ~/.vimrc
+echo - Creating .vim folder
+mkdir ~/.vim
+echo - Copying config files
+cp -r autoload ~/.vim/autoload
+cp -r colors ~/.vim/colors
+echo - All Done!
+echo ----------------------------
